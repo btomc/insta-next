e9a12c9aa79c7d03fd2b25450006ad894ea5d209
Beata Tomczak <tma.btty@gmail.com>
2021-12-27 16:54:05 +0100

    Set up next.js

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className=''>
+      <Head>
+        <title>Insta next</title>
+        <link rel='icon' href='/insta-icon.ico' />
+      </Head>
+      <h1>This is insta clone</h1>
+      {/* Header */}
+      {/* Feed */}
+      {/* Modal */}
+    </div>
+  )
+}
