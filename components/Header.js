f7c6ef2ebc12bbe2214feea1a7c7fad844c0e4f2
Beata Tomczak <tma.btty@gmail.com>
2021-12-27 17:57:23 +0100

    Build the middle side of the Header

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,19 @@
 import Image from 'next/image'
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from '@heroicons/react/outline'
 
 function Header() {
   return (
     <div>
       <div className='flex justify-between max-w-6xl'>
         {/* Left */}
-        <div className='relative hidden lg:inline-grid w-24 h-24 cursor-pointer'>
+        <div className='relative hidden lg:inline-grid w-24 cursor-pointer'>
           <Image
             src='/Instagram_logo.svg.png'
             layout='fill'
@@ -13,11 +21,23 @@ function Header() {
           />
         </div>
 
-        <div className='relative w-10 h-10 lg:hidden flex-shrink-0'>
+        <div className='relative w-10 lg:hidden flex-shrink-0'>
           <Image src='/insta-logo.png' layout='fill' objectFit='contain' />
         </div>
 
         {/* Middle */}
+        <div className='max-w-xs'>
+          <div className='relative mt-1 p-3 rounded-md'>
+            <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
+              <SearchIcon className='h-5 w-5 text-gray-500' />
+            </div>
+            <input
+              className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md'
+              type='text'
+              placeholder='Search'
+            />
+          </div>
+        </div>
 
         {/* Right */}
       </div>
