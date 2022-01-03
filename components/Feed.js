23725606be9834e44437f719f63ef902bbf9d8b7
Beata Tomczak <tma.btty@gmail.com>
2022-01-03 11:50:15 +0100

    Build the MiniProfile component

--- components/Feed.js
+++ components/Feed.js
@@ -1,19 +1,22 @@
-import Posts from "./Posts"
-import Stories from "./Stories"
+import MiniProfile from './MiniProfile'
+import Posts from './Posts'
+import Stories from './Stories'
 
 function Feed() {
   return (
-    <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
+    <main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto'>
       {/* Section */}
-      <section className="col-span-2">
+      <section className='col-span-2'>
         <Stories />
         <Posts />
       </section>
 
       {/* Section */}
-      <section>
-        {/* Mini profile */}
-        {/* Suggestions */}
+      <section className='hidden xl:inline-grid md:col-span-1'>
+        <div className='fixed top-20'>
+          <MiniProfile />
+          {/* Suggestions */}
+        </div>
       </section>
     </main>
   )
