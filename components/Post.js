0fc790d70b124bdd7968455f6343c544c68bf2d5
Beata Tomczak <tma.btty@gmail.com>
2022-01-03 11:32:36 +0100

    Build the InputBox section of the Post

--- components/Post.js
+++ components/Post.js
@@ -44,6 +44,15 @@ function Post({ id, username, userImg, caption, img }) {
       {/* Comments */}
 
       {/* Input box */}
+      <form className='flex items-center p-4'>
+        <EmojiHappyIcon className='h-7' />
+        <input
+          type='text'
+          placeholder='Add a comment...'
+          className='border-none flex-1 focus:ring-0 outline-none'
+        />
+        <button className='font-semibold text-blue-400'>Post</button>
+      </form>
     </div>
   )
 }
