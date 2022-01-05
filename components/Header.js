b393d5b17d75b6f454edcdac8be0b15f5ac053ee
Beata Tomczak <tma.btty@gmail.com>
2022-01-05 11:18:10 +0100

    Implement routers

--- components/Header.js
+++ components/Header.js
@@ -9,9 +9,11 @@ import {
 } from '@heroicons/react/outline'
 import { HomeIcon } from '@heroicons/react/solid'
 import { signIn, signOut, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
 function Header() {
   const { data: session } = useSession()
+  const router = useRouter()
 
   // console.log(session)
 
@@ -19,7 +21,10 @@ function Header() {
     <div className='shadow-sm border-b bg-white sticky top-0 z-50'>
       <div className='flex justify-between max-w-6xl mx-5 lg:mx-auto'>
         {/* Left */}
-        <div className='relative hidden lg:inline-grid w-24 cursor-pointer'>
+        <div
+          onClick={() => router.push('/')}
+          className='relative hidden lg:inline-grid w-24 cursor-pointer'
+        >
           <Image
             src='/Instagram_logo.svg.png'
             layout='fill'
@@ -27,7 +32,10 @@ function Header() {
           />
         </div>
 
-        <div className='relative w-10 lg:hidden flex-shrink-0'>
+        <div
+          onClick={() => router.push('/')}
+          className='relative w-10 lg:hidden flex-shrink-0'
+        >
           <Image src='/insta-logo.png' layout='fill' objectFit='contain' />
         </div>
 
@@ -47,7 +55,7 @@ function Header() {
 
         {/* Right */}
         <div className='flex items-center justify-end space-x-4'>
-          <HomeIcon className='navBtn' />
+          <HomeIcon onClick={() => router.push('/')} className='navBtn' />
           <MenuIcon className='h-6 md:hidden cursor-pointer' />
 
           {session ? (
